8f642c9200bd9d10b121c744cfae77a6381d1c70
Kien Duy <buikienduy2020@gmail.com>
2025-05-09 21:36:09 +0700

    [Thống kê] Ghép tất API thống kê

--- src/application/screen/overview/owner/owner-overview.component.ts
+++ src/application/screen/overview/owner/owner-overview.component.ts
@@ -3,7 +3,9 @@ import { Component, OnInit } from "@angular/core";
 import { NbButtonModule, NbIconModule, NbInputModule, NbTooltipModule } from "@nebular/theme";
 import { OverviewManagement } from "../../../data/management/overview.management";
 import { OverviewService } from "../../../data/service/overview.service";
-import { OverviewStatsModel } from "../../../data/model/overview/overview.model";
+import { LowStockProductModel, OrderCompletionRateModel, OrderStatsModel, OverviewStatsModel, RevenueStatsModel, TopCustomerModel, TopSellingProductModel } from "../../../data/model/overview/overview.model";
+import { GroupDate } from "../../../common/resource/group-date";
+import { OrderStatus } from "../../../common/resource/status";
 
 const NB_LIBS = [
     NbIconModule,
@@ -35,6 +37,13 @@ const PROVIDERS = [
 
 export class OwnerOverviewComponent implements OnInit {
     overviewStats!: OverviewStatsModel;
+    revenueStats!: RevenueStatsModel;
+    orderStats!: OrderStatsModel;
+    topSellingProducts: TopSellingProductModel[] = [];
+    totalCustomers: number = 0;
+    topCustomers: TopCustomerModel[] = [];
+    lowStockProducts: LowStockProductModel[] = [];
+    orderCompletionRate!: OrderCompletionRateModel;
     today = new Date();
     past14Days = new Date();
 
@@ -45,6 +54,12 @@ export class OwnerOverviewComponent implements OnInit {
     async ngOnInit(): Promise<any> {
         this.past14Days.setDate(this.today.getDate() - 14)
         await this.fetchShopOverviewStats();
+        await this.fetchRevenueOvertime();
+        await this.fetchOrderStats();
+        await this.fetchTopSellingProducts();
+        await this.fetchCustomerStats();
+        await this.fetchLowStockProducts();
+        await this.fetchOrderCompletionStats();
     }
 
     async fetchShopOverviewStats() {
@@ -55,4 +70,80 @@ export class OwnerOverviewComponent implements OnInit {
             console.log(error);
         }
     }
+
+    async fetchRevenueOvertime() {
+        try {
+            const response = await this.overviewMana.fetchRevenueOvertime(this.past14Days, this.today, GroupDate.DAY);
+            this.revenueStats = response;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async fetchOrderStats() {
+        try {
+            const response = await this.overviewMana.fetchOrderStats(
+                this.past14Days,
+                this.today,
+                GroupDate.DAY,
+                // OrderStatus.PENDING
+            );
+            this.orderStats = response;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async fetchTopSellingProducts() {
+        try {
+            const response = await this.overviewMana.fetchTopSellingProducts(
+                this.past14Days,
+                this.today,
+                10
+            );
+            this.topSellingProducts = response;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async fetchCustomerStats() {
+        try {
+            const response = await this.overviewMana.fetchCustomerStats(
+                this.past14Days,
+                this.today,
+                10
+            );
+            this.totalCustomers = response.get('totalCustomers') ?? 0;
+            this.topCustomers = response.get('topCustomers') ?? [];
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async fetchLowStockProducts() {
+        try {
+            const minStock = 50;
+            const response = await this.overviewMana.fetchLowStockProducts(minStock);
+            this.lowStockProducts = response;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async fetchOrderCompletionStats() {
+        try {
+            const response = await this.overviewMana.fetchOrderCompletionStats(
+                this.past14Days,
+                this.today,
+                GroupDate.DAY,
+                // OrderStatus.PENDING
+            );
+            this.orderCompletionRate = response;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
 }
