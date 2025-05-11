bcd04041fad45b71d3cd3a891657f74bf7fa3a50
Kien Duy <buikienduy2020@gmail.com>
2025-05-12 01:14:01 +0700

    [Thống kê] Tạo tạm dropdown chọn theo tháng/năm

--- src/application/screen/overview/owner/owner-overview.component.ts
+++ src/application/screen/overview/owner/owner-overview.component.ts
@@ -113,6 +113,29 @@ export class OwnerOverviewComponent implements OnInit {
         { label: 'Năm', value: 'YEAR' }
     ];
 
+    months: Option[] = [
+        { label: 'Tháng 1', value: '1' },
+        { label: 'Tháng 2', value: '2' },
+        { label: 'Tháng 3', value: '3' },
+        { label: 'Tháng 4', value: '4' },
+        { label: 'Tháng 5', value: '5' },
+        { label: 'Tháng 6', value: '6' },
+        { label: 'Tháng 7', value: '7' },
+        { label: 'Tháng 8', value: '8' },
+        { label: 'Tháng 9', value: '9' },
+        { label: 'Tháng 10', value: '10' },
+        { label: 'Tháng 11', value: '11' },
+        { label: 'Tháng 12', value: '12' }
+    ];
+
+    years: Option[] = Array.from({ length: 11 }, (_, i) => ({
+        label: `Năm ${2020 + i}`,
+        value: `${2020 + i}`
+    }));
+
+    selectedMonth: string = '';
+    selectedYear: string = '';
+
     constructor(
         private router: Router,
         private appConfig: AppConfig,
@@ -120,6 +143,11 @@ export class OwnerOverviewComponent implements OnInit {
     ) { }
 
     async ngOnInit(): Promise<any> {
+        const currentMonth = new Date().getMonth() + 1;
+        const currentYear = new Date().getFullYear();
+        this.selectedMonth = currentMonth.toString();
+        this.selectedYear = currentYear.toString();
+
         this.preImage = this.appConfig.getPreImage() ?? '';
         this.pagingLowStock = new PagingModel();
         this.past14Days.setDate(this.today.getDate() - 14);
@@ -472,6 +500,27 @@ export class OwnerOverviewComponent implements OnInit {
 
     onSelectCriteria(criterial: string) {
         this.selectedCriterial = criterial;
+        if (criterial === 'MONTH') {
+            const currentMonth = new Date().getMonth() + 1;
+            this.selectedMonth = currentMonth.toString();
+        } else {
+            this.selectedMonth = '';
+        }
+        if (criterial === 'YEAR') {
+            const currentYear = new Date().getFullYear();
+            this.selectedYear = currentYear.toString();
+        } else {
+            this.selectedYear = '';
+        }
+    }
+
+    onSelectMonth(month: string) {
+        this.selectedMonth = month;
+    }
+
+
+    onSelectYear(year: string) {
+        this.selectedYear = year;
     }
 
     onViewProduct(id: number) {
